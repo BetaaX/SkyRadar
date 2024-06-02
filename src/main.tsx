f41935de1a07410e8a5f63a1cc27567e7c5e5971
Octenx <jota.idk@icloud.com>
2024-06-02 12:56:07 -0300

    feat: adding verification of speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { App } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <SpeedInsights />
         <App />
     </React.StrictMode>
 );
